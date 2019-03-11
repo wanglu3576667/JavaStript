7182ddd3818ccd6fd84f87ea4bf3fbbbff4309af
jayZhou <503177735@qq.com>
2019-03-12 01:06:21 +0800

    添加自动播放功能

--- 苹果轮播（仿制）/main.js
+++ 苹果轮播（仿制）/main.js
@@ -1,13 +1,22 @@
 (function (){
     var $but = $('main>.menu>li')
-    for (let index = 0;index<$but.length;index++){
+    var n = 0;
+    $but.eq(1).addClass('active')
+    for (let index = 1;index<$but.length;index++){
         $but.eq(index).on('click',function(event){
-            var n = $(event.currentTarget).index()-1 //从1开始
-            
-            $('main>.slideShow').eq(0).css({transform: 'translateX('+n*(-920)+'px)',transition:'0.6s'})
-            console.log(event.currentTarget)
+            n = $(event.currentTarget).index()//从1开始
+            $('main>.slideShow').eq(0).css({transform: 'translateX('+(n*(-920)+920)+'px)',transition:'0.6s'})
             $(event.currentTarget).addClass('active').siblings('.active').removeClass('active')
         })
     }
-    console.log(1)
+    var timer = setInterval(function(){
+        if (n === $but.length-1){
+            n=0
+        }
+
+        $but.eq(++n).trigger('click')
+       
+        
+    },3000)
+    
 }())
