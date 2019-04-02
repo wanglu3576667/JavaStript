0f581e8f8b14ad3552ee19919cc0bafb22616890
jayZhou <503177735@qq.com>
2019-04-02 14:29:39 +0800

    已完成

--- 我的FM/JS/FM.js
+++ 我的FM/JS/FM.js
@@ -24,6 +24,7 @@
             this.get_lyric = null
             this.timer = null
             this.hash = null
+            this.state = false
             this.$stop_playing = this.$view.find('.stop_playing')
             this.$song_cover = this.$view.find('.cover')
             this.$singer = this.$view.find('.singer')
@@ -32,18 +33,21 @@
             this.$song_name = this.$view.find('.song_name')
             this.$progress=this.$view.find('.progress')
             this.$time = this.$view.find('.time')
+            this.$loop = this.$view.find('.collect_song')
             this.song = new Audio()
             this.play = false
             this.bindEvent_document()
             this.bindEvent_stop_playing()
             this.bindEvent_progress_star()
             this.bindEvent_progress_pause()
+            this.bindEvent_song_end()
+            this.bindEvent_loop()
             
         },
 
         load_music : function (get_song){
             var _this = contorller ;
-             _this.get_song = get_song
+            _this.get_song = get_song
             _this.song.src =  _this.get_song.song[0].url
             _this.song.autoplay = true
             _this.song.play()
@@ -63,7 +67,7 @@
                _this.hash[value.match(/\d{2}:\d{2}/g)] = value.replace(/\[.+?\]/g,'')
                 
             });
-            _this.hash['00:00'] = '欢迎收听我的FM，我是作者印第安老斑鸠'
+            _this.hash['00:00'] = '欢迎使用音乐播放器，我是印第安老斑鸠'
             
 
         },
@@ -77,38 +81,54 @@
                                                 <use xlink:href="#icon-zantingtingzhi"></use>
                                             </svg>`
                                             )
+                _this.$stop_playing.attr('title','暂停')
             })
         },
 
         bindEvent_stop_playing :function(){
-            this.$stop_playing.on('click',()=>{
-                if(this.play === false){
-                    this.song.pause()
-                    this.$stop_playing.html(`<svg class="icon" aria-hidden="true">
-                                                <use xlink:href="#icon-bofang"></use>
-                                            </svg>`
-                                            )
-                    this.play = true
-                }else{
-                    this.song.play()
-                    this.$stop_playing.html(`<svg class="icon" aria-hidden="true">
-                                                <use xlink:href="#icon-zantingtingzhi"></use>
-                                            </svg>`
-                                            )
-                    this.play = false
-                }
-            })
-
-            this.$next_song.on('click',()=>{
-                this.model.init_song.call( this, this.channelID, this.load_music)
-                this.$stop_playing.html(`<svg class="icon" aria-hidden="true">
-                                                <use xlink:href="#icon-zantingtingzhi"></use>
-                                            </svg>`
-                                            )
-            })
+                this.$stop_playing.on('click',()=>{
+                    if (this.state === true){
+                        if(this.play === false){
+                            this.song.pause()
+                            this.$stop_playing.html(`<svg class="icon" aria-hidden="true">
+                                                        <use xlink:href="#icon-bofang"></use>
+                                                    </svg>`
+                                                    )
+                            this.$stop_playing.attr('title','播放')
+                            this.play = true
+                        }else{
+                            this.song.play()
+                            this.$stop_playing.html(`<svg class="icon" aria-hidden="true">
+                                                        <use xlink:href="#icon-zantingtingzhi"></use>
+                                                    </svg>`
+                                                    )
+                            this.$stop_playing.attr('title','暂停')
+                            this.play = false
+                        }
+                    }else{
+                       $('.state').css({opacity:'1'})
+                    }
+                })
+    
+                this.$next_song.on('click',()=>{
+                    if (this.state === true){
+                        this.model.init_song.call( this, this.channelID, this.load_music)
+                        this.$stop_playing.html(`<svg class="icon" aria-hidden="true">
+                                                        <use xlink:href="#icon-zantingtingzhi"></use>
+                                                    </svg>`
+                                                    )
+                    }else{
+                        $('.state').css({opacity:'1'})
+                    }
+                })
+            
+           
         },
+
         bindEvent_progress_star : function (){
             $(this.song).on('playing',()=>{
+                this.state = true
+                $('.state').css({opacity:'0'})
                 this.timer = setInterval(()=>{
 
                     var currentTime = parseInt(this.song.currentTime,10)
@@ -137,8 +157,36 @@
             $(this.song).on('pause',()=>{
                clearInterval(this.timer) 
             })
+        },
+
+        bindEvent_song_end : function (){
+            $(this.song).on('ended',()=>{
+                this.model.init_song.call( this, this.channelID, this.load_music)
+            })
+        },
+
+        bindEvent_loop : function (){
+            var n = true
+            this.$loop.on('click',()=>{
+                if(n){
+                    n = false
+                    this.song.loop = true
+                    this.$loop.html(`<svg class="icon" aria-hidden="true">
+                                        <use xlink:href="#icon-danquxunhuan"></use>
+                                    </svg>`)
+                    this.$loop.attr('title','单曲循环')
+                }else{
+                    n = true
+                    this.song.loop = false
+                    this.$loop.html(`<svg class="icon" aria-hidden="true">
+                                        <use xlink:href="#icon-yuanxunhuanbofang"></use>
+                                    </svg>`)
+                    this.$loop.attr('title','列表循环')               
+                }   
+               
+            })
         }
-    
+        
 
 
     }
